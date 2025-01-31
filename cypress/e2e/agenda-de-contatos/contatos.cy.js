48444676fc540ce3bee4b06da3f6c34267968ad2
André Scherer Kunz <andreschererkunzt@gmail.com>
2025-01-31 18:42:29 -0300

    fix

--- cypress/e2e/agenda-de-contatos/contatos.cy.js
+++ cypress/e2e/agenda-de-contatos/contatos.cy.js
@@ -5,24 +5,22 @@ describe('Testes utilizando o Cypress', () => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
     });
 
-    const preencherFormulario = (nome, email, telefone) => {
-        cy.get('input[type="text"]').clear().type(nome);
-        cy.get('input[type="email"]').clear().type(email);
-        cy.get('input[type="tel"]').clear().type(telefone);
-    };
-
-    it("Deve adicionar um novo contato", () => {
-        preencherFormulario("Teste", "teste@gmail.com", "1234567890");
+    it("Deve incluir informações no formulário", () => {
+        cy.get('input[type="text"]').type("Teste");
+        cy.get('input[type="email"]').type("teste@gmail.com");
+        cy.get('input[type="tel"]').type("1234567890");
         cy.contains("Adicionar").click();
     });
 
-    it("Deve alterar um contato existente", () => {
-        cy.get(':nth-child(2) .edit').click();
-        preencherFormulario("Teste2", "teste2@gmail.com", "1234567892");
-        cy.contains("Editar").click();
+    it("Deve alterar a informação da lista", () => {
+        cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
+        cy.get('input[type="text"]').clear().type("Teste2");
+        cy.get('input[type="email"]').clear().type("teste2@gmail.com");
+        cy.get('input[type="tel"]').clear().type("1234567892");
+        cy.get(".alterar").click();
     });
 
-    it("Deve remover um contato", () => {
-        cy.get(':nth-child(2) .delete').click();
+    it("Deve remover um contato da lista", () => {
+        cy.get(":nth-child(2) > .sc-gueYoa > .delete").click();
     });
 });
